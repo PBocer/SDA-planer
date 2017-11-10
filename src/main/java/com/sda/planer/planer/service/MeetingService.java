096072472709acdc5e5ba8a2f5676f3680a29960
PBocer <przemek.bocer@gmail.com>
2017-11-10 20:40:55 +0100

    Introduced Meeting feature

---
+++ src/main/java/com/sda/planer/planer/service/MeetingService.java
@@ -0,0 +1,23 @@
+package com.sda.planer.planer.service;
+
+
+import com.sda.planer.planer.model.Meeting;
+import com.sda.planer.planer.model.Room;
+import com.sda.planer.planer.repository.MeetingRepository;
+import com.sda.planer.planer.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MeetingService {
+
+
+    private MeetingRepository meetingRepository;
+
+@Autowired
+    public MeetingService(MeetingRepository meetingRepository){this.meetingRepository= meetingRepository;}
+    public List<Meeting> getAll(){return (List<Meeting>) meetingRepository.findAll();}
+
+}
